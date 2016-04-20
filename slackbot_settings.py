0c1bbd40db9ad7b9e4714ba516e1b0c42adf17c1
koichiro-h <world-daemon@docomo.ne.jp>
2016-04-21 08:47:19 +0900

    Changed API_TOKEN in Setting_py
    Deploying for 1st Project

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-API_TOKEN = "xoxb-31571192182-HX2CgmWUDsi2oi6vq4ChpSwZ"
+#API_TOKEN = "xoxb-31571192182-HX2CgmWUDsi2oi6vq4ChpSwZ"    #testenvironment
+API_TOKEN = "xoxb-36459943460-FwXXjMtN2kkciKQKNlr60bc2"    #test2
 
 default_reply = "スラえもんですが何か？"
 PLUGINS = [
